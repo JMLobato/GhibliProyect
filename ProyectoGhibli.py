2f2de2095eaae3e331fca9916aeb8fd4e725230a
José Manuel Lobato <35557444+JMLobato@users.noreply.github.com>
2018-05-03 10:22:44 +0200

    página de errores

--- ProyectoGhibli.py
+++ ProyectoGhibli.py
@@ -19,7 +19,7 @@ def inicio():
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return 'Parece que algo esta fallando...', 404
+	return render_template("fallo.html"), 404
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', 5000, debug=True)
