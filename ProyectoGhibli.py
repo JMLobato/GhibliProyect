71b9b3cb08cfd362d0d4c06971f1bc0be946242c
José Manuel Lobato <35557444+JMLobato@users.noreply.github.com>
2018-05-11 12:44:41 +0200

    Add files via upload

--- ProyectoGhibli.py
+++ ProyectoGhibli.py
@@ -1,21 +1,32 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+from jinja2 import Template
 import requests
 app = Flask(__name__)
-
+r=requests.get('https://ghibliapi.herokuapp.com/films')
+doc = r.json()
 @app.route('/')
-def inicio(): 
-	return render_template("index.html")
+def inicio():
+	lista=[]
+	for elem in doc:
+		lista.append(elem["title"])
+	return render_template("index.html",lista=lista)
 
-#r=requests.get('https://ghibliapi.herokuapp.com/films')
-#if r.status_code == 200:
-#	doc = r.json()
-#	for peli in doc:
-#		print(peli["title"])
-#		print(peli["director"])
-#		print(peli["producer"])
-#		print(peli["release_date"])
-#		print()
+@app.route('/peliculas')
+def peliculas():
+	return render_template("films.html",doc=doc)
 
+@app.route('/search')
+def search():
+	r=requests.get('https://ghibliapi.herokuapp.com/people')
+	doc2 = r.json()
+	r=requests.get('https://ghibliapi.herokuapp.com/locations')
+	doc3 = r.json()
+	r=requests.get('https://ghibliapi.herokuapp.com/species')
+	doc3 = r.json()
+	r=requests.get('https://ghibliapi.herokuapp.com/vehicles')
+	doc4 = r.json()
+	q=request.form.get("q")
+	return render_template("busqueda.html",doc=doc,doc2=doc2,doc3=doc3,doc4=doc4,q=q)
 
 @app.errorhandler(404)
 def page_not_found(error):
